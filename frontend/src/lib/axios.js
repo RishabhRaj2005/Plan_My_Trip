7b743fdcc64521fcb2866ecdb8cb42a49c4d8aa8
RishabhRaj2005 <rishabhrajfeb22@gmail.com>
2025-06-26 11:26:18 +0530

    changed axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:import.meta.env.MODE==="development"?"http://localhost:5002/api": "/api",
+    baseURL:"https://plan-my-trip-hd4g.onrender.com",
     withCredentials:true,
 })
