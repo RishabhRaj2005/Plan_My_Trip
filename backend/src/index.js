4b00bdd633093eb7eb676e5963430e7de42d06dd
RishabhRaj2005 <rishabhrajfeb22@gmail.com>
2025-06-26 11:36:50 +0530

    updated cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin:  ["http://localhost:5173", "https://guileless-rolypoly-f838a7.netlify.app"],
         credentials: true,
     })
 )
